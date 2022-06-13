f419327966d801240aa744e937d588236c7849ee
fali <fali@nisum.com>
2022-06-13 13:45:57 +0500

    completed redux tutorial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "@reduxjs/toolkit";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const store = configureStore({
+//   reducer: allReducer,
+// });
+
+const store = configureStore({
+  reducer: allReducer,
+  // To disable devtools in production
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
